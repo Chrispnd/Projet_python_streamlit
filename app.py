10ceb61c74eb329b73617002dd0ab0bd25450c9a
Chris.pnd <pinaud846@gmail.com>
2023-11-21 18:10:41 +0100

    ajout de la sidebar

--- app.py
+++ app.py
@@ -7,23 +7,24 @@ st.write("""
 Space Repetition System
 """)
 
-option = st.selectbox(
-    "What would you like to review ,",
-    ("Joins", "Groupby", "Window functions"),
-    index=None,
-    placeholder="Select a thing"
-)
-
-st.write('You selected', option)
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to review ?",
+        ("Joins", "Groupby", "Window functions"),
+        index=None,
+        placeholder="Select a thing"
+    )
+    st.write('You selected', option)
 
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
+tab1, tab2, tab3 = st.tabs(["Cat","Dog","Own"])
 
-sql_query = st.text_area(label="entrez votre input")
-try:
-    result=db.query(sql_query).df()
-    st.write(f"Vous avez entré la query suivante: {sql_query}")
-    st.dataframe(result)
-except:
-    st.write("Vous n'avez pas encore rentré de réponse")
-
+with tab1:
+    sql_query = st.text_area(label="entrez votre input")
+    try:
+        result=db.query(sql_query).df()
+        st.write(f"Vous avez entré la query suivante: {sql_query}")
+        st.dataframe(result)
+    except:
+        st.write("Vous n'avez pas encore rentré de réponse")
