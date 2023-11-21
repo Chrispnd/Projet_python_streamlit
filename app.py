68b13bff20aad03535854145bd54198e4ae3bb38
Chris.pnd <pinaud846@gmail.com>
2023-11-21 17:13:19 +0100

    ajout d'un try except pour vérifier si l'utilisateur a entré une réponse

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
 
 sql_query = st.text_area(label="entrez votre input")
-result=db.query(sql_query).df()
-st.write(f"Vous avez entré la query suivante: {sql_query}")
-st.dataframe(result)
+try:
+    result=db.query(sql_query).df()
+    st.write(f"Vous avez entré la query suivante: {sql_query}")
+    st.dataframe(result)
+except:
+    st.write("Vous n'avez pas encore rentré de réponse")
+
